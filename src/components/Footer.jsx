c8d54f77c229fd3d69ab0971808994270ccfda31
D-future0 <oka4collins@gmail.com>
2025-06-29 08:16:42 -0700

    fix: add hyperlink to developer's name in Footer component for improved accessibility

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="w-full flex flex-col text-center p-4 bg-gray-800 text-white">
-        <p className='text-xs mb-2'>Developed by Okafor Ifechukwu Collins</p>
+        <p className='text-xs mb-2'><a href='https://okafor-i-collins-portfolio-next-js-collins-projects-7836abdc.vercel.app' target='_blank'>Developed by Okafor Ifechukwu Collins</a></p>
         <p>© 2025 PowerLab</p>
       </footer>
   )
